a26c26d26bd807144d52e7df2b147ecc77eb554d
Mario Costabile <cstmra00t05c349p@studenti.unical.it>
2025-03-20 18:08:39 +0100

    Migration to JWT

--- VeraPelle-front/src/app/app.module.ts
+++ VeraPelle-front/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
 import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
 import {KeycloakService} from './services/keycloak/keycloak.service';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './header/header/header.component';
 
 export function kcFactory(kcService: KeycloakService){
   return () => kcService.init();
@@ -15,11 +16,13 @@ export function kcFactory(kcService: KeycloakService){
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     HttpClient,{
